27bf1d7fe1f2ef082c67a7c0beec199562700b1a
Joe Chung <joechung-msft@users.noreply.github.com>
2025-08-22 17:03:13 -0400

    Respond with 415 if Content-Type is not text/plain

--- api-rocket/src/main.rs
+++ api-rocket/src/main.rs
@@ -6,10 +6,19 @@ use rocket::http::{ContentType, Status};
 use rocket::serde::json::json;
 use shared_lib::{parse, pretty_print_token};
 
-#[post("/api/v1/parse", data = "<input>", format = "text/plain")]
+#[post("/api/v1/parse", data = "<input>")]
 async fn parse_endpoint(
     input: Data<'_>,
+    content_type: Option<&ContentType>,
 ) -> Result<(ContentType, String), (Status, (ContentType, String))> {
+    if content_type != Some(&ContentType::Plain) {
+        let err_json = json!({ "code": 415, "message": "Unsupported Media Type" });
+        return Err((
+            Status::UnsupportedMediaType,
+            (ContentType::JSON, err_json.to_string()),
+        ));
+    }
+
     let bytes = input.open(128.kibibytes()).into_bytes().await;
 
     let text = match bytes {
